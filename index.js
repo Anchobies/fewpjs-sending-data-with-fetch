37bfc9252fdf4c438bb9f1e13c2062d5360aa02a
Anchobies <anthonychung97@gmail.com>
2021-05-23 13:59:46 -0400

    commit tree dfeeacba7c9d6d113d1b4205d3a0a4f6af16f9b5

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Add your code here
+let submitData = (name, email) => {
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            name: name,
+            email: email
+        })
+    })
+    .then(response => response.json())
+    .then(data => {
+        document.body.insertAdjacentHTML("beforeend", `<div>
+            <p>${data.name}</p>
+            <p>${data.email}</p>
+            <p>${data.id}</p>
+        </div>`)
+    })
+    .catch(error => {
+        document.body.innerHTML += `<p>${error.message}</p>`;
+    })
+}
+
